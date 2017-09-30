6e8f1a235b989b0d726576f10a93f68006454c46
adham bishr <adhambishr@gmail.com>
2017-09-29 18:39:53 -0700

    added geocomplete feature

--- js/zomatoAPI.js
+++ js/zomatoAPI.js
@@ -1,6 +1,26 @@
 $(document).ready(function() {
   var apiKey = "5fe006d31daf49c4fb1bc8bfdfd5d5d5";
 
+  //Complete location for submit location
+  $("#place-to-eat").geocomplete();
+
+  //Press enter button to submit location
+  $("html").keypress(function(event) {
+    if (event.keyCode == 13 || event.which == 13) {
+      event.preventDefault();
+      console.log($("#place-to-eat").val());
+    }
+  });
+  //Retrieve value from text inbox & convert to button
+  $("#find-place").on("click", function(event) {
+    // event.preventDefault() prevents the form from trying to submit itself.
+    // We're using a form so that the user can hit enter instead of clicking the button if they want
+    event.preventDefault();
+
+    //Give button an attribute to recall when searching
+    console.log($("#place-to-eat").val());
+  });
+
   var queryURL = "https://developers.zomato.com/api/v2.1/search?";
 
   queryURL += $.param({
